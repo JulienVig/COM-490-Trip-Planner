bfc489189e7a1dbb005f93d8f1fa1f401f739143
Julien Roger Lucien Ben Haim <julien.benhaim@epfl.ch>
2022-05-29 12:43:58 +0000

    Change display of route name and trans type for walk

--- scripts/graph.py
+++ scripts/graph.py
@@ -243,15 +243,15 @@ class RealSolution:
                 curr_station_dep = prev_node
                 curr_route_name = n.route_name
                 curr_dep_time = RealSolution._convert_time_to_rw(n.arr_time, target_arr_time)
-                curr_trans_type = 'transport'
+                curr_trans_type = n.transport_type
                 curr_n_stops_crossed = 1
 
             # Case beginning of a walking trip
             elif isinstance(n, WalkingStop):
                 curr_station_dep = prev_node
-                curr_route_name = 'walk_route'
+                curr_route_name = ''
                 curr_dep_time = RealSolution._convert_time_to_rw(n.arr_time, target_arr_time)
-                curr_trans_type = 'walk'
+                curr_trans_type = 'Walk'
                 curr_n_stops_crossed = 1
 
             # Case end of a trip
